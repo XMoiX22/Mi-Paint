9af66447ed2509e980351046b96a4ef0669c8ba1
XMoiX22 <moisesjimez1234@gmail.com>
2022-02-07 01:12:45 -0500

    Guardar, Traduccion, Icon, Y correccion errors

---
+++ MiPaint_ar.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Principal</name>
+    <message>
+        <location filename="principal.ui" line="17"/>
+        <source>Principal</source>
+        <translation>المالك</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="31"/>
+        <source>Archivo</source>
+        <translation>أرشيف</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="40"/>
+        <source>Estilo</source>
+        <translation>نمط</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="48"/>
+        <source>Forma</source>
+        <translation>شكل</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="63"/>
+        <source>Nuevo</source>
+        <translation>الجديد</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="66"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="71"/>
+        <source>Guardar</source>
+        <translation>حفظ</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="74"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="79"/>
+        <source>Salir</source>
+        <translation>اخرج</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="82"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="87"/>
+        <source>Color</source>
+        <translation>اللون</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="90"/>
+        <source>Ctrl+Shift+C</source>
+        <translation>Ctrl+Shift+S</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="95"/>
+        <source>Ancho</source>
+        <translation>واسع</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="98"/>
+        <source>Ctrl+Shift+A</source>
+        <translation>Ctrl+Shift+A</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="103"/>
+        <source>Lineas</source>
+        <translation>خطوط</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="106"/>
+        <source>Ctrl+L</source>
+        <translation>Ctrl+L</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="114"/>
+        <source>Libre</source>
+        <translation>مجانا</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="117"/>
+        <source>Ctrl+Shift+L</source>
+        <translation>Ctrl+Shift+L</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="122"/>
+        <source>Rectánculos</source>
+        <translation>المستطيلات</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="125"/>
+        <source>Ctrl+R</source>
+        <translation>Ctrl+R</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="130"/>
+        <source>Circunferencias</source>
+        <translation>محيطات</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="133"/>
+        <source>Ctrl+C</source>
+        <translation>Ctrl+C</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="138"/>
+        <source>Relleno</source>
+        <translation>حشوة</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="141"/>
+        <source>Ctrl+Shift+R</source>
+        <translation>Ctrl+Shift+R</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="81"/>
+        <location filename="principal.cpp" line="86"/>
+        <location filename="principal.cpp" line="90"/>
+        <location filename="principal.cpp" line="105"/>
+        <source>Número de líneas: </source>
+        <translation>عدد الخطوط:</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="122"/>
+        <source>Ancho del pincel</source>
+        <translation>عرض الفرشاة</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="123"/>
+        <source>Ingrese el ancho del pincel de dibujo</source>
+        <translation>أدخل عرض فرشاة الرسم</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="137"/>
+        <source>Color del pincel</source>
+        <translation>لون الفرشاة</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="150"/>
+        <location filename="principal.cpp" line="156"/>
+        <location filename="principal.cpp" line="160"/>
+        <source>Guardar imagen</source>
+        <translation>حفظ الصور</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="152"/>
+        <source>Imágenes (*.png)</source>
+        <translation>الصور (* .png)</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="157"/>
+        <source>Archivo almacenado en: </source>
+        <translation>الملف المخزن في:</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="161"/>
+        <source>No se pudo almacenar la imagen.</source>
+        <translation>لا يمكن تخزين الصورة</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="183"/>
+        <source>Color de relleno</source>
+        <translation>لون التعبئة</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include "principal.h"
 
 #include <QApplication>
+#include <QTranslator>
+#include <QInputDialog>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // Objeto para manejar las traducciones
+    QTranslator traducion;
+    // Solicitando al usuario que seleccione un idioma
+    QStringList idiomas;
+    idiomas << "Arabe" << "Inglés" << "Español";
+    QString idiomaSeleccionado = QInputDialog::getItem(NULL,
+                               "Idioma",
+                               "Seleccione un idioma",
+                               idiomas);
+    // Dependiendo del idioma seleccionado, carga el archivo de rtaducción
+    if (idiomaSeleccionado == "Arabe"){
+        traducion.load(":/MiPaint_ar.qm");
+    }else if (idiomaSeleccionado == "Inglés"){
+        traducion.load(":/MiPaint_en.qm");
+    }
+    // Si es diferente de español, se instala la traducción en TODA la aplicación
+    if (idiomaSeleccionado != "Español"){
+        a.installTranslator(&traducion);
+    }
+    // Muestra la ventana principal
     Principal w;
     w.show();
     return a.exec();
